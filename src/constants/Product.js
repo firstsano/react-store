7ee38a47a44ed34f8b4f57b59a17acbf76b1eb49
Alexander Koltunov <firstsano@gmail.com>
2018-07-20 19:25:38 +0300

    Add products example

---
+++ src/constants/Product.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        imageUrl: '/web/img/wiedzmin-1.png',
+        title: 'The Last Wish',
+        price: 300
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-2.png',
+        title: 'Sword of Destiny',
+        price: 330
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-3.png',
+        title: 'Blood of Elves',
+        price: 420
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-4.png',
+        title: 'Time of Contempt',
+        price: 290
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-5.png',
+        title: 'Baptism of Fire',
+        price: 310
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-6.png',
+        title: 'The Tower of the Swallow',
+        price: 410
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-7.png',
+        title: 'The Lady of the Lake',
+        price: 370
+    },
+    {
+        imageUrl: '/web/img/wiedzmin-8.png',
+        title: 'Season of Storms',
+        price: 385
+    },
+];
+
+export default products;
