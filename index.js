553215ee1ce4107f62b30715b6e4086074d9e703
Alexander Koltunov <firstsano@gmail.com>
2018-07-20 19:05:51 +0300

    Remove deprecations, add App class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import 'typeface-roboto';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './src/Hello';
+import App from './src/App';
 
 ReactDOM.render(
-    <Hello />,
+    <App />,
     document.getElementById('root')
 );
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CssBaseLine from '@material-ui/core/CssBaseline';
+
+class App extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <CssBaseLine />
+                <div> Test </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
